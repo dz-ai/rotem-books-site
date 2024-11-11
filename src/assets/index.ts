babb0a81ba1212b1be355c999f0b1205ef7388d5
david-zim <davidzim29.90@gmail.com>
2024-11-11 12:40:54 +0100

    Add - favicon icon.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,3 @@
 // consider each image.png file as a module and export it under custom name
-export {default as logo} from './rotem-logo.jpg';
+export {default as logo} from './rotem-logo.png';
 export {default as googleLogo} from './google_on_white_hdpi.png';
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,8 +4,7 @@ import './footer.css';
 const Footer: React.FC = () => {
     return (
         <footer>
-            <a href="https://www.vecteezy.com/free-vector/kids-book-logo" target="_blank">Kids Book Logo Vectors by Vecteezy</a>
-            <p>&copy; 2024 Children's Bookstore. All rights reserved.</p>
+            <p>&copy; כל הזכויות שמורות</p>
         </footer>
     );
 }
