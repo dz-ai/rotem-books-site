5fc7218bf45e1bc489c5748af87e0082087b6c88
david-zim <davidzim29.90@gmail.com>
2024-11-14 21:37:24 +0100

    Add - transparent background beside the sidebar that is displayed as the sidebar opens. And close the sidebar as the user click on it. In addition, as the user clicks on back/forward browser buttons, the sidebar closes.

--- src/components/sideNavBar/sideNavBar.tsx
+++ src/components/sideNavBar/sideNavBar.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './sideNavBar.css';
-import {NavLink, useNavigate} from 'react-router-dom';
+import {NavLink, useLocation, useNavigate} from 'react-router-dom';
 import {logo} from '../../assets';
 import {useCart} from "../../context/cartContext.tsx";
 import {GiShoppingCart} from "react-icons/gi";
@@ -8,6 +8,7 @@ import {GiShoppingCart} from "react-icons/gi";
 const SideNavBar = () => {
     const cartContext = useCart();
     const navigate = useNavigate();
+    const location = useLocation();
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -19,6 +20,25 @@ const SideNavBar = () => {
         navigate(path);
         setIsOpen(!isOpen);
     }
+
+    // add listener to back/forward browser button to close the sidebar
+    // as the user click on them but without to navigate back or forward.
+    useEffect(() => {
+
+        const handleBackButton = () => {
+
+            if (isOpen) {
+                navigate(location.pathname);
+                setIsOpen(false);
+            }
+        };
+
+        window.addEventListener('popstate', handleBackButton);
+
+        return () => {
+            window.removeEventListener('popstate', handleBackButton);
+        }
+    }, [isOpen]);
 
     return (
         <div className="side-bar-container">
@@ -41,6 +61,7 @@ const SideNavBar = () => {
                     <li onClick={() => onNavBtnClicked('/contact-page')}>צור קשר</li>
                 </ul>
             </nav>
+            <div className={`side-bar-background ${isOpen ? 'nav-open' : ''}`} onClick={toggleNav}></div>
             <NavLink to="/">
                 <img className="logo" src={logo} alt="לוגו קיפוד עם גיטרה"/>
             </NavLink>
